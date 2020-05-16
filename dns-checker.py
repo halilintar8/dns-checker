af048eb6247d14a13df18bd06863d325d58e1016
antoniodvr <antoniodvr@gmail.com>
2020-05-16 16:07:53 +0200

    Add bulk whois

--- dns-checker.py
+++ dns-checker.py
@@ -1,28 +1,35 @@
 #!/usr/bin/env python
 # DNS checker
 
+from tqdm import tqdm
 import whois
 import argparse
 import sys
 from domain import Domain
 
-def print_heading():
+def print_whois_results(available_domains, taken_domains):
     """
-       Pretty print heading
+       Pretty print the whois results
     """
-    print("%-25s  %-35s  %-25s  %-4s" % ("DOMAIN NAME", "REGISTRAR", "EXPIRATION DATE", "DAYS LEFT"))
-
-def print_whois(whois_data):
-    """
-       Pretty print the domain information
-    """
+    if len(available_domains) > 0:
+        print("\nAvailable domains:")
+    else:
+        print("\nAll the domains have been taken!")
     
-    print("%-25s  %-35s  %-25s  %-s" % (
-        whois_data.name, 
-        (whois_data.registrar[:30] + '...') if len(whois_data.registrar) > 30 else whois_data.registrar, 
-        whois_data.expiration_date, 
-        whois_data.expiration_days
-    ))
+    for available_domain in available_domains:
+        print("  > %s" % available_domain)
+    
+    if len(taken_domains) > 0:
+        print("\nTaken domains:")
+        print("%-25s  %-35s  %-25s  %-4s" % ("DOMAIN NAME", "REGISTRAR", "EXPIRATION DATE", "DAYS LEFT"))
+
+    for taken_domain in taken_domains:
+        print("%-25s  %-35s  %-25s  %-s" % (
+            taken_domain.name, 
+            (taken_domain.registrar[:30] + '...') if len(taken_domain.registrar) > 30 else taken_domain.registrar, 
+            taken_domain.expiration_date, 
+            taken_domain.expiration_days
+        ))
 
 def is_blank_or_comment(s):
     """ function to check if a line
@@ -32,22 +39,27 @@ def is_blank_or_comment(s):
     # return true if a line starts with #
     return not s.strip() or s.startswith('#')
 
-def execute_whois(domain_name):
+def bulk_whois(domains):
     """
-       Execute domain name whois
+       Execute domains name whois
     """
-    try:
-        whois_data = whois.query(domain_name)
-    except Exception as e:
-        print("Failed to whois the domain name '%s'. Exception %s" % (domain_name, e))
-        sys.exit(1)
+    available_domains = []
+    taken_domains = []
+    domains = tqdm(domains, unit="domain", ncols=100)
+    for domain in domains:
+        domains.set_description(domain)
+        try:
+            whois_data = whois.query(domain)
+        except Exception as e:
+            pass
 
-    if whois_data is not None:
-        whois_data.__class__ = Domain
-    else:
-        whois_data = Domain(domain_name)
+        if whois_data is not None:
+            whois_data.__class__ = Domain
+            taken_domains.append(whois_data)
+        else:
+            available_domains.append(domain)
 
-    return whois_data
+    return (available_domains, taken_domains)
 
 def parse_cli_args():
     """
@@ -63,20 +75,22 @@ def parse_cli_args():
     
 def main():
     args = parse_cli_args()
+    print("")
 
-    print_heading()
-
+    domains = []
     if args.file:
         with open(args.file, "r") as lines:
             for line in lines:
+                line = line.rstrip()
                 if is_blank_or_comment(line):
                     continue
-                domain_name = line
-                whois_data = execute_whois(domain_name)
-                print_whois(whois_data)
+                domains.append(line)
     elif args.domain:
-        whois_data = execute_whois(args.domain)
-        print_whois(whois_data)
+        domains.append(args.domain)
+    
+    (available_domains, taken_domains) = bulk_whois(domains)
+    taken_domains.sort(key=lambda domain: domain.expiration_days, reverse=False) 
+    print_whois_results(available_domains, taken_domains)
         
 if __name__ == "__main__":
     main()  
